b77d109c172bde4fe159beb552646bbd34c1d535
EC2 Default User <ec2-user@ip-172-31-34-101.ap-northeast-1.compute.internal>
2020-03-06 13:10:57 +0000

    user-microposts-fix-gemfile-2

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,10 +7,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # if Rails.env.production?
-    storage :fog
-  else
+  #   storage :fog
+  # else
     storage :file
-  end
+  # end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
